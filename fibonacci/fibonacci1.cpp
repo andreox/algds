8f6a698fc4d68fa14eb3b48f9cc1beb326afcbf2
andreox <andreozzi.alessio@gmail.com>
2017-02-02 19:24:03 +0100

    Create fibonacci1.cpp

---
+++ fibonacci/fibonacci1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std ;
+
+int fibonacci( int n ) {
+
+	if ( n <= 2 ) return 1 ;
+	else return ( fibonacci(n-1)+fibonacci(n-2)) ;
+
+}
+
+int main() {
+
+	cout << fibonacci(6) << endl ;
+	return 0 ;
+
+}
